09317fd6928e4f0ce9181f00c9bd6fad61089756
Abdelfattah <boutala11@gmail.com>
2017-11-24 16:08:05 +0000

    ops

--- GestionEvenementsEnsa/src/main/java/com/ensa/GestionEvenementsEnsaApplication.java
+++ GestionEvenementsEnsa/src/main/java/com/ensa/GestionEvenementsEnsaApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GestionEvenementsEnsaApplication {
-//moppppppppppppppp
+//ops
 	public static void main(String[] args) {
 
 		SpringApplication.run(GestionEvenementsEnsaApplication.class, args);
