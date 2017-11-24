14ac241086a8bdbf107358a5b144d26ac79c7239
Abdelfattah <boutala11@gmail.com>
2017-11-24 19:15:14 +0000

    ok ok

--- GestionEvenementsEnsa/src/main/java/com/ensa/GestionEvenementsEnsaApplication.java
+++ GestionEvenementsEnsa/src/main/java/com/ensa/GestionEvenementsEnsaApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GestionEvenementsEnsaApplication {
-//no no
+//no ok ol
 	public static void main(String[] args) {
 
 		SpringApplication.run(GestionEvenementsEnsaApplication.class, args);
