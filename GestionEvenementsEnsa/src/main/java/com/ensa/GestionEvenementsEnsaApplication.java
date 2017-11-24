8cebf368b2076329f03e9314ada40f6218f7ce2f
Sony <Sony@Sony-PC>
2017-11-24 16:09:21 +0000

    opsops

--- GestionEvenementsEnsa/src/main/java/com/ensa/GestionEvenementsEnsaApplication.java
+++ GestionEvenementsEnsa/src/main/java/com/ensa/GestionEvenementsEnsaApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GestionEvenementsEnsaApplication {
-//ops
+//opsops
 	public static void main(String[] args) {
 
 		SpringApplication.run(GestionEvenementsEnsaApplication.class, args);
